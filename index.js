992710ef5146f4907a191d3187941d6d9b3c16f9
Victor Fernandes <90485435+VictorFMag@users.noreply.github.com>
2024-09-09 10:59:56 -0300

    Commit 6 - Fatura em HTML

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const { readFileSync } = require('fs');
 
+function gerarFaturaHTML(fatura, pecas) {
+  let faturaHTML = `<html>\n`;
+  faturaHTML += `<p>Fatura ${fatura.cliente}</p>\n`;
+  faturaHTML += `<ul>\n`;
+
+  for (let apre of fatura.apresentacoes) {
+    faturaHTML += `  <li>${getPeca(pecas, apre).nome}: ${formatarMoeda(calcularTotalApresentacao(pecas, apre))} (${apre.audiencia} assentos)</li>\n`;
+  }
+
+  faturaHTML += `</ul>\n`;
+  faturaHTML += `<p>Valor total: ${formatarMoeda(calcularTotalFatura(pecas, fatura.apresentacoes))}</p>\n`;
+  faturaHTML += `<p>Créditos acumulados: ${calcularTotalCreditos(pecas, fatura.apresentacoes)}</p>\n`;
+  faturaHTML += `</html>`;
+
+  return faturaHTML;
+}
+
 function formatarMoeda(valor) {
   return new Intl.NumberFormat("pt-BR",
     {
@@ -74,4 +91,6 @@ function gerarFaturaStr(fatura, pecas) {
 const faturas = JSON.parse(readFileSync('./faturas.json'));
 const pecas = JSON.parse(readFileSync('./pecas.json'));
 const faturaStr = gerarFaturaStr(faturas, pecas);
+const faturaHTML = gerarFaturaHTML(faturas, pecas);
 console.log(faturaStr);
+console.log(faturaHTML);
