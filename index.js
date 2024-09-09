c20297af5aedd5d0af6dd5344ce3bf4a2446e40a
Victor Fernandes <90485435+VictorFMag@users.noreply.github.com>
2024-09-09 10:24:11 -0300

    Commit 2 - Replace Temp with Query

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ function gerarFaturaStr(fatura, pecas) {
       minimumFractionDigits: 2
     }).format;
 
+  // função query
+  function getPeca(apresentacao) {
+    return pecas[apresentacao.id];
+  }
 
   function calcularTotalApresentacao(apre, peca) {
     let total = 0;
@@ -35,17 +39,17 @@ function gerarFaturaStr(fatura, pecas) {
   }
 
   for (let apre of fatura.apresentacoes) {
-    const peca = pecas[apre.id];
-    
-    let total = calcularTotalApresentacao(apre, peca);
+    // const peca = getPeca(apre);
+
+    let total = calcularTotalApresentacao(apre, getPeca(apre));
 
     // créditos para próximas contratações
     creditos += Math.max(apre.audiencia - 30, 0);
-    if (peca.tipo === "comedia")
+    if (getPeca(apre).tipo === "comedia")
       creditos += Math.floor(apre.audiencia / 5);
 
     // mais uma linha da fatura
-    faturaStr += `  ${peca.nome}: ${formato(total / 100)} (${apre.audiencia} assentos)\n`;
+    faturaStr += `  ${getPeca(apre).nome}: ${formato(total / 100)} (${apre.audiencia} assentos)\n`;
     totalFatura += total;
   }
   faturaStr += `Valor total: ${formato(totalFatura / 100)}\n`;
